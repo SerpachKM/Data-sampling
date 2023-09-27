e9b4279fa7217c083d7e1ee7f9b249a58140c348
SerpachKM <131816710+SerpachKM@users.noreply.github.com>
2023-09-27 13:57:34 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,54 +1,69 @@
-INSERT INTO artist_list(id, name)
-VALUES	(1, 'artist_1'),
-	    (2, 'artist_2'),
-	    (3, 'artist_3'),
-	    (4, 'artist_4'),
-	    (5, 'artist_5'),
-	    (6, 'artist_6'),
-	    (7, 'artist_7'),
-	    (8, 'artist_8'),
+INSERT INTO genres
+(genre_name)
+VALUES
+('Rap'), 
+('Pop'), 
+('Rock'), 
+('Punk');
 
-INSERT INTO Genre_list(id, Genre_Name)
-VALUES	(1, 'genre_1'),
-        (2, 'genre_2'),
-        (3, 'genre_3'),
-        (4, 'genre_4'),
-        (5, 'genre_5');
-    
+INSERT INTO executors
+(executor_name)
+VALUES
+('Ramstein'), 
+('Linkin Park'), 
+('Король и шут'), 
+('Баста'), 
+('Zivert');
 
-INSERT INTO album_List(id, album_name, release_date)
-VALUES	(1, 'album_name_1', '2016-01-01'),
-	    (2, 'album_name_2', '2015-01-01'),
-	    (3, 'album_name_3', '2017-01-01'),
-	    (4, 'album_name_4', '2018-01-01'),
-	    (5, 'album_name_5', '2018-06-01'),
-	    (6, 'album_name_6', '2018-12-31'),
-	    (7, 'album_name_7', '2020-01-01'),
-	    (8, 'album_name_8', '2020-01-01');
+INSERT INTO genres_executors
+(genre_id, executor_id)
+VALUES
+(1, 5),
+(2, 4),
+(3, 1),
+(4, 3),
+(3, 2);
 
-INSERT INTO Track_List(id, track_name, track_time)
-VALUES	(1, 'track_1', 3.00),
-	    (2, 'track_2', 3.10),
-	    (3, 'track_3', 3.20),
-	    (4, 'track_4', 3.30),
-	    (5, 'track_5', 3.40),
-	    (6, 'track_6', 3.50),
-	    (7, 'track_7', 4.00),
-	    (8, 'track_8', 4.10),
-	    (9, 'track_9', 4.20),
-	    (10, 'track_10', 4.30),
-	    (11, 'track_11', 4.40),
-	    (12, 'track_12', 4.50),
-	    (13, 'track_13', 5.00),
-	    (14, 'track_14', 5.10),
-	    (15, 'my', 5.20);
+INSERT INTO albums
+(album_name, album_year)
+VALUES
+('Будь как дома путник', 1997),
+('БАСТА 1', 2006), 
+('Vinyl #2', 2021), 
+('Rammstein', 2019), 
+('Meteora', 2003);
 
-INSERT INTO collection_List(id, collection_name, release_date)
-VALUES	(1, 'collection_1', '2017-01-01'),
-	    (2, 'collection_2', '2016-01-01'),
-	    (3, 'collection_3', '2017-01-01'),
-	    (4, 'collection_4', '2018-01-01'),
-	    (5, 'collection_5', '2018-07-01'),
-	    (6, 'collection_6', '2019-01-01'),
-	    (7, 'collection_7', '2020-12-31'),
-	    (8, 'collection_8', '2021-01-01');
+INSERT INTO tracks
+(track_name, track_duration, album_id)
+VALUES
+('Forever Young', 180, 3), 
+('Я устал', 228, 1), 
+('Rammstein', 206, 4), 
+('Pushing My Away', 191, 5), 
+('Вечно молодой Мой', 268, 2), 
+('Numb', 187, 5);
+
+INSERT INTO executors_albums
+(executor_id, album_id)
+VALUES
+(1, 4), 
+(2, 5), 
+(3, 1), 
+(5, 3), 
+(4, 2);
+
+INSERT INTO collections
+(collection_name, collection_year)
+VALUES
+('Наивные песни', 2005), 
+('Поколение Брат', 2022), 
+('Lost Highway', 1997), 
+('Valentine', 2001);
+
+INSERT INTO collections_tracks
+(collection_id, track_id)
+VALUES
+(1, 1), 
+(2, 2), 
+(3, 4), 
+(4, 5);
